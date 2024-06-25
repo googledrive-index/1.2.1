88a13b02fbcc3efc82532430d9c636fc3445300c
googledrive-index <154752758+googledrive-index@users.noreply.github.com>
2024-06-25 11:19:38 +0530

    Create pika-redirect.js

---
+++ pika-redirect.js
@@ -0,0 +1,1 @@
+var referrerDomain=document.referrer.split("/")[2],allowedDomains=["pikasub.com","www.pikasub.com"];-1===allowedDomains.indexOf(referrerDomain)&&location.replace("https://pikasub.com");
